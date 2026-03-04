4b2093b8d2f542324e579bfe42e9aec83e01acd5
Andy Chen <andrewr.chen@gmail.com>
2026-03-04 12:13:58 -0800

    stratified split + working binary classifier

---
+++ models/binary_classifier.py
@@ -0,0 +1,85 @@
+import torch
+from torchvision.models import resnet18, ResNet18_Weights
+import torch.nn as nn
+from poison_dataset import BinaryPoisonDataset
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Subset, DataLoader
+
+# ====================================== data loading =========================================================
+clean_dir = "../data_generation/output_data/dog_features"
+poison_dir = "../data_generation/output_data/poisoned_dog"
+
+dataset = BinaryPoisonDataset(clean_dir, poison_dir, skip_clean=50)
+
+labels = [label for _, label in dataset.samples]
+
+# stratified split: 80 train, 20 test
+train_idx, test_idx = train_test_split(
+    range(len(dataset.samples)),
+    test_size=0.2,
+    stratify=labels,
+    random_state=67
+)
+
+train_dataset = Subset(dataset, train_idx)
+test_dataset  = Subset(dataset, test_idx)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader  = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+
+
+# ====================================== model config =========================================================
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+weights = ResNet18_Weights.DEFAULT
+model = resnet18(weights=weights)
+model.fc = nn.Linear(model.fc.in_features, 2)   # binary
+model = model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+
+
+# ====================================== training =========================================================
+epochs = 5
+
+for epoch in range(epochs):
+    model.train()
+    running_loss = 0.0
+
+    for imgs, labels in train_loader:
+        imgs, labels = imgs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(imgs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss/len(train_loader):.4f}")
+
+print("Training complete")
+
+
+# ====================================== model eval =========================================================
+model.eval()
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for imgs, labels in test_loader:
+        imgs, labels = imgs.to(device), labels.to(device)
+
+        outputs = model(imgs)
+        preds = torch.argmax(outputs, dim=1)
+
+        correct += (preds == labels).sum().item()
+        total += labels.size(0)
+
+accuracy = 100 * correct / total
+print(f"\nBinary classification accuracy: {accuracy:.2f}%")
+print(f"Number of Samples: {total}")
---
+++ models/poison_dataset.py
@@ -0,0 +1,54 @@
+import os
+import pickle
+import numpy as np
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+
+class BinaryPoisonDataset(Dataset):
+    def __init__(self, clean_dir, poison_dir, skip_clean=20):
+        self.samples = []
+
+        self.transform = transforms.Compose([
+            transforms.Resize((224,224)),
+            transforms.Lambda(lambda img: img.convert("RGB")),
+            transforms.ToTensor()
+        ])
+
+        # clean data
+        clean_files = sorted([f for f in os.listdir(clean_dir) if f.endswith(".p")])
+        clean_files = clean_files[skip_clean:]   # remove duplicates
+        print(len(clean_files))
+
+        for f in clean_files:
+            self.samples.append((os.path.join(clean_dir, f), 0))
+
+        # poisoned data
+        poison_files = sorted([f for f in os.listdir(poison_dir) if f.endswith(".p")])
+        poison_files = poison_files[:skip_clean]
+        print(len(poison_files))
+        for f in poison_files:
+            self.samples.append((os.path.join(poison_dir, f), 1))
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        path, label = self.samples[idx]
+
+        with open(path, "rb") as f:
+            data = pickle.load(f)
+
+        img = data["img"]
+
+        # numpy to PIL
+        if isinstance(img, np.ndarray):
+            if img.dtype != np.uint8:
+                img = ((img + 1) * 127.5).clip(0,255).astype(np.uint8)
+            img = Image.fromarray(img)
+
+        # if already PIL, do nothing
+        img = self.transform(img)
+
+        return img, torch.tensor(label, dtype=torch.long)
