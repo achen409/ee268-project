bf9b7e61c77f5eb922f76748d321ca3515e4b4cf
Andy Chen <andrewr.chen@gmail.com>
2026-03-04 14:58:18 -0800

    return losses

--- models/binary_classifier.py
+++ models/binary_classifier.py
@@ -8,8 +8,8 @@ from sklearn.metrics import confusion_matrix, classification_report
 from torch.utils.data import Subset, DataLoader
 
 # TODO: move model training & eval to function(s)
-# TODO: training -> params: train_loader, test_loader, debug=False, poison_ratio, num_epochs. return model
-# TODO: test -> params: model. return report_dict
+# TODO: training -> params: train_loader, test_loader, debug=False, poison_ratio, num_epochs. return model, losses
+# TODO: test -> params: model, debug=False. return report_dict, cm
 # ====================================== data loading =========================================================
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -18,7 +18,7 @@ clean_dir  = os.path.join(script_dir, "..", "data_generation", "output_data", "d
 poison_dir = os.path.join(script_dir, "..", "data_generation", "output_data", "poisoned_dog")
 
 # TODO: test varying poisoning ratios (ex. 1%, 5%, 10%)
-dataset = BinaryPoisonDataset(clean_dir, poison_dir, poison_ratio=60)
+dataset = BinaryPoisonDataset(clean_dir, poison_dir, poison_ratio=10)
 
 labels = [label for _, label in dataset.samples]
 
@@ -43,7 +43,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 weights = ResNet18_Weights.DEFAULT
 model = resnet18(weights=weights)
-model.fc = nn.Linear(model.fc.in_features, 2)   # binary
+model.fc = nn.Linear(model.fc.in_features, 2) # binary
 model = model.to(device)
 
 criterion = nn.CrossEntropyLoss()
@@ -53,6 +53,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
 # ====================================== training =========================================================
 epochs = 15
+losses = []
 # TODO: more epochs since it works so fast? varying epochs?
 
 for epoch in range(epochs):
@@ -67,11 +68,11 @@ for epoch in range(epochs):
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-        # TODO: control of loss (keep track of poison ratio per batch)
 
         running_loss += loss.item()
 
     print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss/len(train_loader):.4f}")
+    losses.append(running_loss/len(train_loader))
 
 print("Training complete")
 
@@ -100,7 +101,7 @@ print(cm)
 # more metrics
 # precision: TP / (TP + FP)
 # recall: TP / (TP + FN)
-report_dict = classification_report(all_labels, all_preds, target_names=["Clean", "Poison"], output_dict=True) # TODO: return when functionized later
+report_dict = classification_report(all_labels, all_preds, target_names=["Clean", "Poison"], output_dict=True)
 report_str = classification_report(all_labels, all_preds, target_names=["Clean", "Poison"])
 print("\nClassification Report:")
 print(report_str)
